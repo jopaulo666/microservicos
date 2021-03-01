f3706fcc20e1632f3eb3f3294451fbc99aad69e1
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-03-01 20:17:49 -0300

    Balanceamento de carga com Ribbon

--- rh-pagamento/src/main/java/com/jopaulo/rhpagamento/feignclients/TrabalhadorFeignClient.java
+++ rh-pagamento/src/main/java/com/jopaulo/rhpagamento/feignclients/TrabalhadorFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.jopaulo.rhpagamento.entities.Trabalhador;
 
 @Component
-@FeignClient(name = "rh-trabalhador", url = "localhost:8001", path = "/trabalhadores")
+@FeignClient(name = "rh-trabalhador", path = "/trabalhadores")
 public interface TrabalhadorFeignClient {
 
 	@GetMapping(value = "/{id}")
--- rh-pagamento/src/main/java/com/jopaulo/rhpagamento/resources/PagamentoResource.java
+++ rh-pagamento/src/main/java/com/jopaulo/rhpagamento/resources/PagamentoResource.java
@@ -1,6 +1,9 @@
 package com.jopaulo.rhpagamento.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +18,18 @@ import com.jopaulo.rhpagamento.service.PagamentoService;
 public class PagamentoResource {
 	
 	@Autowired
+	private static Logger logger = LoggerFactory.getLogger(PagamentoResource.class);
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private PagamentoService service;
 	
 	@GetMapping(value = "/{trabalhadorId}/dias/{dias}")
 	public ResponseEntity<Pagamento> getPagamento(@PathVariable Long trabalhadorId, @PathVariable Integer dias){
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Pagamento pagamento  = service.getPagamento(trabalhadorId, dias);
 		return ResponseEntity.ok(pagamento);
 	}
