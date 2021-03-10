16613ee7d60967945807c143a86281a021f789f5
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-03-09 23:54:46 -0300

    Usuario, Role, associação muitos para muitos

---
+++ rh-usuario/src/main/java/com/jopaulo/rhusuario/entities/Role.java
@@ -0,0 +1,31 @@
+package com.jopaulo.rhusuario.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleNome;
+}
---
+++ rh-usuario/src/main/java/com/jopaulo/rhusuario/entities/Usuario.java
@@ -0,0 +1,46 @@
+package com.jopaulo.rhusuario.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private String senha;
+	
+	@ManyToMany(fetch = FetchType.EAGER) // carrega os dados dos perfis junto com o usuário
+	@JoinTable(name = "tb_usuario_role",
+		joinColumns = @JoinColumn(name = "usuario_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id")			
+	)
+	private Set<Role> roles = new HashSet<>();
+}
