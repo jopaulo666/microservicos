a2fd16c8a571bb006fadb707ec134302d3747428
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-03-16 11:44:13 -0300

    Criando o projeto rh-oauth

---
+++ rh-aouth/src/main/java/com/jopaulo/rhoauth/RhAouthApplication.java
@@ -0,0 +1,13 @@
+package com.jopaulo.rhoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhAouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhAouthApplication.class, args);
+	}
+
+}
