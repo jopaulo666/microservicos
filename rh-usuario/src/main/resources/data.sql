a823dc4e2e1dce8be46dd2961ecfe592e336caab
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-03-18 14:51:07 -0300

    Autorização pelo Zuul

--- rh-usuario/src/main/resources/data.sql
+++ rh-usuario/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO tb_usuario (nome, email, senha) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$5llfu5.8vivzoTwbfnTJ6.l0Rm4b1YIt.NM48NBi9YydpAneAX2mC');
 INSERT INTO tb_usuario (nome, email, senha) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$5llfu5.8vivzoTwbfnTJ6.l0Rm4b1YIt.NM48NBi9YydpAneAX2mC');
 
-INSERT INTO tb_role (role_nome) VALUES ('ROLE_OPERADOR');
+INSERT INTO tb_role (role_nome) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (role_nome) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_usuario_role (usuario_id, role_id) VALUES (1, 1);
